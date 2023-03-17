1ac826fee60cf9a1c91b7820816a4a4e3315141d
Jung Sang Wung <kk5949@naver.com>
2023-03-17 16:01:39 +0900

    스탭,호텔,예약 마이그레이션 생성

---
+++ database/migrations/2023_03_17_065109_create_reservations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->integer("user_id");
+            $table->integer("hotel_id");
+            $table->integer("step")->default("10")->comment("10:진행중 20:예약완료 30:예약반려 40:예약취소");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
